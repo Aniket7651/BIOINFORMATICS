0c9d44c5b476dc8c184431dbf05c6bc0585d632f
ANIKET YADAV <aniketyadav8687@gmail.com>
2022-05-19 01:04:07 +0530

    removing django, adding tkinter

---
+++ pyCrossbill/_U_Interface/main.py
@@ -0,0 +1,7 @@
+from logging import root
+from tkinter import *
+
+root = Tk()
+text = Text(root, height=100, width=100).pack()
+# t.insert(END, "python hello Tk!")
+root.mainloop()
--- pyCrossbill/_U_Interface/pyCrossbill/pyCrossbill/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# create views here:
-
-def first(request):
-    return render(request, "first.html")
